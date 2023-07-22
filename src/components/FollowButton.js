46a57b3822a2954a4cd71da55618a6cef33298dd
Madhav Pandey <pmadhav279@gmail.com>
2023-07-22 23:35:29 +1000

    User can follow and unfollow other users

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "./contexts/AuthContext";
-import { updateDoc, doc , arrayUnion} from "firebase/firestore";
+import { updateDoc, doc , arrayUnion, arrayRemove} from "firebase/firestore";
 import { db } from "../fireConfig";
+import Dropdown from 'react-bootstrap/Dropdown';
+
 
 const FollowButton = ({ target_user_id})=>{
 
@@ -9,9 +11,16 @@ const[isFollowing, setIsFollowing] = useState(false);
 const {currentUser} = useAuth();
 
 
-const handleUnfollow = ()=>{
+const handleUnfollow =  async ()=>{
 
+await updateDoc(doc(db,"Users",currentUser.id ),{
+    following:arrayRemove(target_user_id)
+});
+await updateDoc(doc(db,"Users", target_user_id),{
+    followers: arrayRemove(currentUser.id)
+});
 
+setIsFollowing(false);
 }
 
 const handleFollow = async()=>{
@@ -36,13 +45,28 @@ useEffect(()=>{
 const unscribe =()=>{
 
 currentUser.following.includes(target_user_id) ? setIsFollowing(true) : setIsFollowing(false);
-console.log(currentUser.following.includes(target_user_id));
 }
 return unscribe;
 },[])
     return<>
 <div>
-    {isFollowing? <button  className="btn btn-secondary btn-sm">Unfollow</button>: <button onClick={handleFollow} className="btn btn-sm btn-primary">Follow</button>}
+    {isFollowing? <>
+        <Dropdown>
+      <Dropdown.Toggle variant="secondary" size="sm" id="dropdown-basic">
+        Following
+      </Dropdown.Toggle>    
+      <Dropdown.Menu>
+
+      <Dropdown.Item>
+        <button  onClick={handleUnfollow} className="btn btn-danger btn-sm w-100">Unfollow</button>
+      </Dropdown.Item>
+
+      </Dropdown.Menu>
+
+</Dropdown>
+    
+    </>
+    : <button onClick={handleFollow} className="btn btn-sm btn-primary">Follow</button>}
 </div>
     </>
 }
